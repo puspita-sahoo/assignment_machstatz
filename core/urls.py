bd7f76da4b68eb19eac06bf6447d514a93f383ac
puspita-sahoo <85501280+puspita-sahoo@users.noreply.github.com>
2021-09-24 02:16:36 +0530

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -16,16 +16,5 @@ urlpatterns = [
     path('production/count', views.production_count, name="production_count"),
     path('machines/data', views.machine_data, name="machine_data"),
     path('belts/avg', views.belts_avg, name="belts_avg"),
-<<<<<<< HEAD
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-=======
-    # re_path(r'^belt/$', views.belt)
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-
->>>>>>> b3c4c0737ba351f30adbe43645691b5f7f16cfb4
-
-
-
-
 
