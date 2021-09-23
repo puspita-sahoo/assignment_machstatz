3746aed6d252d3863458d5a29b26ce487a105ea1
puspita-sahoo <85501280+puspita-sahoo@users.noreply.github.com>
2021-09-23 23:59:00 +0530

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -4,6 +4,11 @@ from django.contrib import admin
 from django.urls import path, re_path
 from . import views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,17 +16,9 @@ urlpatterns = [
     path('machines/data', views.machine_data, name="machine_data"),
     path('belts/avg', views.belts_avg, name="belts_avg"),
     # re_path(r'^belt/$', views.belt)
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-"""
-(r'^get_item/$', get_item)
-And in your view:
 
-def get_item(request):
-    id = int(request.GET.get('id'))
-    type = request.GET.get('type', 'default')
-    
-"""
 
 
 
